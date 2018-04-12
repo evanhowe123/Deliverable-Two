2a584eaeaf8fbcb842c8f96472ff24efbd8742b4
Evan <evan.r.howe@gmail.com>
2018-04-12 11:59:55 -0400

    Initial commit 	new file:   index.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+import java.util.scanner; 
+
+
+public class SumofNumbers
+	public static void main( String [] args ){
+		Scanner scanner = new Scanner( System.in );
+		int usernumber = -1;
+		int sum;
+		
+		while(userNumber < 1 ) {
+			System.out.println( "Enter a Number!")
+			userNumber = Scanner.nextint()
+		}
+		
+		for( int number = 1; number<= userNumber; number++ ) {
+			 sum = sum + number;
+			 
+		}
+		
+		
+		System.out.println( "Enter a Number!")
+		userNumber = Scanner.nextint()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
