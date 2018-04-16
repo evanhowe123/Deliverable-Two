9106d9148d289baebd5d0620a9de4d22bdfd7f80
Evan <evan.r.howe@gmail.com>
2018-04-16 07:32:18 -0400

    modified:   index.html  modified:   script.js

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-import java.util.scanner; 
+<SCRIPT Language="JavaScript">
 
+function showAndClearField(frm){
+  if (frm.num.value == "")
+      alert("Hey! You didn't enter anything!")
+  else{
+  		var result = 0;
+		var input = frm.num.value; 
+		for (var i = 1; i <= input; i++) {
+		  result += i;
+		}
+      alert("Result: " + result)
+  }
+  frm.firstName.value = ""
+}
 
-public class SumofNumbers
-	public static void main( String [] args ){
-		Scanner scanner = new Scanner( System.in );
-		int usernumber = -1;
-		int sum;
-		
-		while(userNumber < 1 ) {
-			System.out.println( "Enter a Number!")
-			userNumber = Scanner.nextint()
-		}
-		
-		for( int number = 1; number<= userNumber; number++ ) {
-			 sum = sum + number;
-			 
-		}
-		
-		
-		System.out.println( "Enter a Number!")
-		userNumber = Scanner.nextint()
+</SCRIPT>
+	
+
+	
+<script type="text/javascript">
+confirm("Want to play?");
+	var userinput;
+	do{
+	userinput = prompt("Want to Play?");
+	if (userinput =="yes"){
+		prompt("Say Something")
+	} else if (userinput !="yes" && userinput !="no"){
+			console.log(userinput)
+	
 		
 		
 		
@@ -29,6 +37,8 @@ public class SumofNumbers
 		
 		
 		
-		
+	///////////
+
+	
 		
 		
